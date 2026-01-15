ba4f81d53137cf55b6f11ced7f0c657df72b02b4
motty <motty.mio2@gmail.com>
2026-01-15 13:00:45 +0000

    fix: Apply flake8 Q000 fixes to legged_gym_math/__init__.py
    Corrected double quotes to single quotes in mevius/mevius_utils/legged_gym_math/__init__.py to comply with flake8 Q000 rule.

--- mevius/mevius_utils/legged_gym_math/__init__.py
+++ mevius/mevius_utils/legged_gym_math/__init__.py
@@ -1,7 +1,7 @@
 from .legged_gym_math import quat_apply_yaw, torch_rand_sqrt_float, wrap_to_pi
 
 __all__ = [
-    "quat_apply_yaw",
-    "torch_rand_sqrt_float",
-    "wrap_to_pi",
+    'quat_apply_yaw',
+    'torch_rand_sqrt_float',
+    'wrap_to_pi',
 ]
